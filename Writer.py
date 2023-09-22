7cb51b783c2d787bb41679ddd27e6c6279872d5a
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2023-09-23 00:11:29 +0900

    :wastebasket: delete: writing conditions
    常に書き込むようにした

--- Writer.py
+++ Writer.py
@@ -14,16 +14,8 @@ class FrameDataWriter:
         self.lf = open(self.LIMIT_FILE, 'w')
 
     def write(self, arg):
-        if self.speed == -1:
-            self.speed = arg.speed
-        if self.limit == -1 and arg.limit >= 10:
-            self.limit = arg.limit
-        if self.distance == -1:
-            self.distance = arg.distance
-
-        if (abs(self.speed - arg.speed) < 1) and (arg.limit >= 10) and (abs(self.distance - arg.distance) < 10):
-            self.speed = arg.speed
-            self.distance = arg.distance
-            self.limit = arg.limit
-            self.sf.write("{},{}\n".format(self.distance, self.speed))
-            self.lf.write("{},{}\n".format(self.distance, self.limit))
+        self.speed = arg.speed
+        self.distance = arg.distance
+        self.limit = arg.limit
+        self.sf.write("{},{}\n".format(self.distance, self.speed))
+        self.lf.write("{},{}\n".format(self.distance, self.limit))
