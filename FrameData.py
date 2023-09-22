6b63192c5405077e449fc2ab7f1b033dce5332a5
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2023-09-22 23:47:49 +0900

    :bug: fix: type of param
    speed, distance, limitの型を文字列からfloat, float intに変換

--- FrameData.py
+++ FrameData.py
@@ -12,14 +12,17 @@ class FrameData:
             match_speed = re.search(r"(\d+(\.\d+)?)", lines[0])
             if match_speed:
                 self.speed = match_speed.group(1)
+                self.speed = float(self.speed)
 
             match_distance = re.search(r"(\d+(\.\d+)?)", lines[3])
             if match_distance:
                 self.distance = match_distance.group(1)
+                self.distance = float(self.distance)
 
             match_limit = re.search(r"(\d+(\.\d+)?)", lines[1])
             if match_limit:
                 self.limit = match_limit.group(1)
+                self.limit = int(self.limit)
 
     def to_string(self):
         return "{}km/h, {}m, [{}]".format(self.speed, self.distance, self.limit)
